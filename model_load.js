055a6660659bcff5c1a2250a61884a5ff8038652
KangYoungSeo <22019112024@dgu.ac.kr>
2022-11-01 15:26:45 +0900

    update

---
+++ model_load.js
@@ -0,0 +1,11 @@
+import * as tf from '@tensorflow/tfjs'
+import * as sk from 'scikitjs'
+
+const tf = require('@tensorflow/tfjs-node')
+const sk = require('scikitjs')
+
+sk.setBackend(tf)
+
+
+
+var scikit = require('scikit-learn');
